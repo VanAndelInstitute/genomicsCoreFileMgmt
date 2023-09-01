8577b8ec751f66975ecf1e5a5a361dc486f9ba1a
zackramjan <zack.hr@gmail.com>
2023-09-01 11:07:03 -0400

    notify to correct email

--- notify.pl
+++ notify.pl
@@ -19,8 +19,8 @@ for my $d (@dirs)
 	my $out = `ls $d`;
 	my $outfull = "$d:\n" . `ls -l $d`;
     #email("marie.adams\@vai.org,ben.johnson\@vai.org,zack.ramjan\@vai.org","$d needs to be cleaned",$outfull) if length($out) > 5;
-    #email("sequencing-notifications\@vai.org","$d needs to be cleaned",$outfull) if length($out) > 5;
-    email("zack.ramjan\@vai.org","$d needs to be cleaned", $outfull) if length($out) > 5;
+    email("sequencing-notifications\@vai.org","$d needs to be cleaned",$outfull) if length($out) > 5;
+    #email("zack.ramjan\@vai.org","$d needs to be cleaned", $outfull) if length($out) > 5;
 }
 
 
