ff3e4f580883e3b60410c0e1c4239ea86eadc606
zackramjan <zack.hr@gmail.com>
2024-02-06 11:30:24 -0500

    added aviti

---
+++ rsync_instruments_aviti.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if [[ ! -d /varidata/research/instruments/sequencing/Aviti/AV234602 ]] ; then
+	echo 'Aviti dir is not there, aborting.'
+	exit
+fi
+
+cd  /varidata/research/instruments/sequencing/Aviti/AV234602
+
+mkdir -p .trash
+find 2* -maxdepth 0 -mtime +60 -type d -exec mv {} .trash \; 
+chown -R marie.adams:sequencing-technology .trash
